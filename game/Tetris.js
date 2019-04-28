3bb8ba4fd3ab0c7ab2b349e3d5e96ca8318ce5c7
Otter <me44888@gmail.com>
2019-04-28 13:26:39 +0300

    Tetris-like piece generation. You can move left and right

--- game/Tetris.js
+++ game/Tetris.js
@@ -1,12 +1,12 @@
 const ROW = 20;
 const COL = 10;
 const SQ = 20;
-const VAC = "white"
+const VAC = "white";
 
 const cvs = document.getElementById("tetris");
-const ctx = cvs.getContext("2d");
+const ctx = cvs.getContext('2d');
 
-let board = []
+let board = [];
 
 function createBoard() {
   for (var r = 0; r < ROW; r++){
@@ -44,8 +44,30 @@ const PIECE = [
   [T, "grey"],
   [O, "grey"]
 ];
+var usedPieces = [];
+var availablePieces = [0, 1, 2, 3, 4, 5, 6];
+arrShuffle(availablePieces);
 
-let p = new Piece(PIECE[0][0], PIECE[0][1]);
+function arrShuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+function randomPiece(){
+  if (availablePieces.length == 0) {
+    availablePieces = [...usedPieces];
+    arrShuffle(availablePieces);
+    usedPieces = [];
+  }
+
+  console.log(availablePieces);
+  var r = availablePieces[0];
+  usedPieces.push(r);
+  availablePieces.shift();
+
+  return new Piece(PIECE[r][0],PIECE[r][1]);
+}
+
+let p = randomPiece();
 
 function Piece(shape, color) {
   this.shape = shape;
@@ -53,18 +75,55 @@ function Piece(shape, color) {
   this.shapeN = 0;
   this.active = this.shape[this.shapeN];
 
-  this.x = 0;
+  this.x = 3;
   this.y = 0;
 }
 
-Piece.prototype.draw = function () {
+Piece.prototype.fill = function (color) {
   for (var r = 0; r < this.active.length; r++){
     for (var c = 0; c < this.active.length; c++) {
       if(this.active[r][c]) {
-        drawSquare(this.x+c, this.y+r, this.color);
+        drawSquare(this.x+c, this.y+r, color);
       }
     }
   }
 }
 
+Piece.prototype.draw = function () {
+  this.fill(this.color);
+}
+
+Piece.prototype.undraw = function () {
+  this.fill(VAC);
+}
+
+Piece.prototype.moveLeft = function () {
+  this.undraw();
+  this.x--;
+  this.draw();
+}
+
+Piece.prototype.moveRight = function () {
+  this.undraw();
+  this.x++;
+  this.draw();
+}
+
+Piece.prototype.debugNewPiece = function () {
+  p.undraw();
+  p = randomPiece();
+  p.draw();
+}
+
 p.draw();
+
+document.addEventListener("keydown", CONTROL);
+function CONTROL(event){
+    if(event.keyCode == 37) {
+        p.moveLeft();
+    }else if(event.keyCode == 39) {
+        p.moveRight();
+    }else if(event.keyCode == 67) {
+        p.debugNewPiece();
+    }
+}
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 5000);
+
+const PORT = 5000
+
+app.set('port', PORT);
 app.use('/game', express.static(__dirname + '/game'));
 
 
@@ -15,6 +18,15 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'Tetris.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+
+io.on('connection', function(socket){
+  console.log('User Connected');
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
+  });
+});
+
+
+server.listen(PORT, function() {
+  console.log('Starting server on port ' + PORT);
 });
